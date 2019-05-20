21c1c4582fa34a0f86177b1280965999f1249fbd
buergerling <michael.buergerling@gmail.com>
2019-05-20 23:11:34 +0200

    change

--- example/example.dart
+++ example/example.dart
@@ -33,14 +33,50 @@ TailRec<bool> isOdd(List<int> xs) {
   }
 }
 
+int startRecursion(int n) {
+  TailRec<int> recursiveFunction(int z) {
+    if (z < n) {
+      return tailcall(() => recursiveFunction(z + 1));
+    } else {
+      return done(z);
+    }
+  }
+
+  return recursiveFunction(0).compute();
+}
+
+int startRecursion2(int n) {
+  int recursiveFunction(int z) {
+    if (z < n) {
+      return recursiveFunction(z + 1);
+    } else {
+      return z;
+    }
+  }
+
+  return recursiveFunction(0);
+}
+
 void main() {
   // Example 1
-  var res = fib(20).result();
-  print("Result: $res");
+  //var res = fib(20).result();
+  //print("Result: $res");
 
   // Example 2
-  List<int> r = rangeList(1, 40002);
-  print(r.last);
-  bool res2 = isEven(r).result();
-  print(res2);
+  //List<int> r = rangeList(1, 40002);
+  //print(r.last);
+  //bool res2 = isEven(r).result();
+  //print(res2);
+
+  // Example 3
+  //List<int> r = rangeList(1, 40002);
+  //print(r.last);
+  //bool res2 = isEven(r).result();
+  //print(res2);
+  print("begin 1");
+  var res = startRecursion(100000000);
+  print("end: " + res.toString());
+  print("begin 2");
+  var res2 = startRecursion2(10000);
+  print("end: " + res2.toString());
 }
